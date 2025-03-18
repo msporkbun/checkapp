630a55a1a6b0c03eea21138e0ebec20b4ce4164a
Patty <pattamon.nakvi@gmail.com>
2025-03-18 21:19:30 +1100

    Building the interface for login and signup screen, using navigation link to switch between the page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,14 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -18,7 +17,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
@@ -32,9 +31,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      // Stack: Navigation stack means it only shows one screen at a time, using
-      tab
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      {/* Stack: Navigation stack means it only shows one screen at a time, using tab */}
       {/* <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,26 +1,91 @@
 // Code: Signup screen
 // Using Expo, you need to export the RootLayout component from app/_layout.tsx and use it in app/signup.tsx. This will provide the theme and navigation context to the app. The RootLayout component will also handle the splash screen and font loading. It works as a shell for the app.
 
-import { View, Text, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validPassword, setValidPassword] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
+
+  useEffect(() => {
+    // check password length
+    if (password.length >= 8) setValidPassword(true);
+    else setValidPassword(false);
+  }, [password]);
+
+  useEffect(() => {
+    if (email.includes('@') && email.indexOf('@') > 0) {
+      setValidEmail(true);
+    } else {
+      setValidEmail(false);
+    }
+  }, [email]);
+
+
   return (
-    <View style={styles.container}>
-      // Label: Signup screen
-      <Text>Create Account</Text>
-      // Button: Link to login screen
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Create an account</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        secureTextEntry={true}
+        value={password}
+      />
+      <Pressable>
+        <Text>Sign up </Text>
+      </Pressable>
+
       <Link href="/">
         <Text>Back to Login Page</Text>
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#E2D9D9',
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 50,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    color: 'black',
+    // borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    margin: 5,
+    borderWidth: 1,
+  },
+
 });
